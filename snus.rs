ac165988f75c538dd274553ef8f39f48cf7dd0a2
horooga <saintnurlan@gmail.com>
2025-04-07 22:27:33 +0300

    3

--- snus.rs
+++
@@ -1,6 +0,0 @@
-
-kddddk.set({ "v" }, ",v", "<Esc>", { noremap = true, silent = true })
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.set({ "v" }, ",v", "<Esc>", { noremap = true, silent = true })
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.set({ "v" }, ",v", "<Esc>", { noremap = true, silent = true })
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk.set({ "v" }, ",v", "<Esc>", { noremap = true, silent = true })
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
